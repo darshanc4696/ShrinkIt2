486623b255c86876c8ef1a961310ba9f9e5a3605
Darshan <darshanc4696@gmail.com>
2025-01-27 11:31:59 +0530

    DOne

--- url-shortener-react/src/components/LandingPage.jsx
+++ url-shortener-react/src/components/LandingPage.jsx
@@ -30,10 +30,10 @@ const LandingPage = () => {
             transition={{ duration: 0.8 }}
             className="font-bold font-roboto text-slate-800 md:text-5xl sm:text-4xl text-3xl   md:leading-[55px] sm:leading-[45px] leading-10 lg:w-full md:w-[70%] w-full"
           >
-            Linklytics Simplifies URL Shortening For Efficient Sharing.
+            ShrinkIt Simplifies URL Shortening For Efficient Sharing.
           </motion.h1>
           <p className="text-slate-700 text-sm my-5">
-            Linklytics streamlines the process of URL shortening, making sharing
+            ShrinkIt streamlines the process of URL shortening, making sharing
             links effortless and efficient. With its user-friendly interface,
             Linklytics allows you to generate concise, easy-to-share URLs in
             seconds. Simplify your sharing experience with Linklytics today.
